3fb1a17f1dd9c9602636f9f8e2cf84d302cb4e7b
keaneflynn <kmflynn24@berkeley.edu>
2022-03-20 00:54:59 -0700

    random file generator

---
+++ train-test_generator.py
@@ -0,0 +1,43 @@
+from ast import parse
+from cgi import test
+import os
+import argparse
+import random
+
+def outputFiles(training_data, training_percent, prefix_dir):
+    image_list = []
+    for file in os.listdir(training_data):
+        if file.endswith('.txt'):
+            continue
+        image_list.append(file)
+    total_images = len(image_list)
+    random.shuffle(image_list)
+    training_data_total = round(total_images*training_percent)
+    testing_data_total = total_images - training_data_total
+    training_data_list = image_list[training_data_total:]
+    testing_data_list = image_list[:testing_data_total]
+
+    with open('train.txt', 'w') as f:
+        for data in training_data_list:
+            filename = prefix_dir+data
+            #print(filename)
+            f.write(filename+'\n')
+
+    with open('test.txt', 'w') as k:
+        for data in testing_data_list:
+            filename = prefix_dir+data
+            k.write(filename+'\n')
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('training_data', type=str, help='path/to/training/data')
+    parser.add_argument('training_percent', type=float, default=0.80, help='percentage of data for training')
+    parser.add_argument('prefix_dir', type=str, help='image/prefix/')
+    args = parser.parse_args()
+    outputFiles(training_data=args.training_data, 
+                training_percent=args.training_percent,
+                prefix_dir=args.prefix_dir)
+
+if __name__ == '__main__':
+    main()
