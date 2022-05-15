6b77d613f60d3a4b9cef4c64fa9cd4257c008f67
keaneflynn <kmflynn24@berkeley.edu>
2022-05-15 16:22:54 -0700

    fix to training and test data generator

--- train-test_generator.py
+++ train-test_generator.py
@@ -1,5 +1,3 @@
-from ast import parse
-from cgi import test
 import os
 import argparse
 import random
@@ -14,13 +12,13 @@ def outputFiles(training_data, training_percent, prefix_dir):
     random.shuffle(image_list)
     training_data_total = round(total_images*training_percent)
     testing_data_total = total_images - training_data_total
-    training_data_list = image_list[training_data_total:]
+    training_data_list = image_list[testing_data_total:]
     testing_data_list = image_list[:testing_data_total]
+    
 
     with open('train.txt', 'w') as f:
         for data in training_data_list:
             filename = prefix_dir+data
-            #print(filename)
             f.write(filename+'\n')
 
     with open('test.txt', 'w') as k:
@@ -32,7 +30,7 @@ def outputFiles(training_data, training_percent, prefix_dir):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('training_data', type=str, help='path/to/training/data')
-    parser.add_argument('training_percent', type=float, default=0.80, help='percentage of data for training')
+    parser.add_argument('training_percent', type=float, default=0.80, help='percentage of data for training (ex 0.80)')
     parser.add_argument('prefix_dir', type=str, help='image/prefix/')
     args = parser.parse_args()
     outputFiles(training_data=args.training_data, 
